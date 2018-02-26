aa85ce0f0d46b0c794fbff6404dfb218eb804074
YOUGG <yougg@macbook-pro-de-yougg.local>
2018-02-26 12:31:00 +0100

    Initial Commit

---
+++ contact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  contact
+//
+//  Created by YOUGG on 2/26/18.
+//  Copyright © 2018 YOUGG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
